930fd4560578c7e2db358d64bece97bd2393b02f
Polina Zorko <polina.zorko@mail.ru>
2021-04-30 23:52:00 +0300

    add serializers

---
+++ ser_create/json_serializer.py
@@ -0,0 +1,42 @@
+import json
+from inspect import stack
+from .my_load import load as ld
+from .my_dump import dump as dp
+
+
+class JsonSerializer:
+
+    one_ld = True
+    
+    def loads(self, string):
+        if self.one_ld:
+            stack_step = 1
+        else:
+            stack_step = 2
+        self.one_ld = True
+
+        ld_dict = json.loads(string)
+        obj = ld(ld_dict, stack()[stack_step][0].f_globals)
+        return obj
+
+
+    def dumps(self, obj):
+        dump_dict = dp(obj)
+        return json.dumps(dump_dict, indent=4)
+        
+
+    def load(self, fp):
+        fp.seek(0)
+        string = fp.read()
+        self.one_ld = False
+        return self.loads(string)  
+        
+
+    def dump(self, obj, fp):
+        string = self.dumps(obj)
+        fp.write(string)
+        fp.flush()
+
+
+    def __str__(self):
+        return 'JSON serializer'
---
+++ ser_create/pickle_serializer.py
@@ -0,0 +1,42 @@
+import pickle
+from inspect import stack
+from .my_load import load as ld
+from .my_dump import dump as dp
+
+class PickleSerializer:
+    
+    one_ld = True
+    
+    def loads(self, bin_string):
+        if self.one_ld:
+            stack_step = 1
+        else:
+            stack_step = 2
+        self.one_ld = True
+        
+        ld_dict = pickle.loads(bin_string)
+        obj = ld(ld_dict, stack()[stack_step][0].f_globals)
+        return obj
+
+
+    def dumps(self, obj):
+        dump_dict = dp(obj)
+        return pickle.dumps(dump_dict)
+
+
+    def load(self, fp):
+        fp.seek(0)
+        bin_string = fp.read()
+        self.one_ld = False
+        return self.loads(bin_string)  
+
+
+    def dump(self, obj, fp):
+        bin_string = self.dumps(obj)
+        fp.write(bin_string)
+        fp.flush()
+
+
+    def __str__(self):
+        return 'Pickle serializer'
+
