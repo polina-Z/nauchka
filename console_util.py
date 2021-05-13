6ca315c62d31a3bcdcfe3c13844270def19b7ec7
Polina Zorko <polina.zorko@mail.ru>
2021-05-13 22:22:48 +0300

    update console utility

--- console_util.py
+++ console_util.py
@@ -1,51 +1,53 @@
 import converter
 import argparse
-import logging 
+import logging
 
 logging.basicConfig(
     level=logging.WARNING, filename="warning.log", format="%(levelname)s: %(message)s"
 )
 
-try:
-    parser = argparse.ArgumentParser(description='Serializer', fromfile_prefix_chars='@')
-
-    parser.add_argument(
-        'input_path',
-        metavar="input_path",
-        type=str,
-        help="Path to the converted file"
+def main():
+    try:
+        parser = argparse.ArgumentParser(
+            description="Serializer", fromfile_prefix_chars="@"
         )
 
-    parser.add_argument(
-        '-il',
-        '--in_lang',
-        metavar='input_language',
-        choices=['json', 'yaml', 'pickle', 'toml'],
-        default='json',
-        type=str,
-        help='Override language for input file'
+        parser.add_argument(
+            "input_path", metavar="input_path", type=str, help="Path to the converted file"
         )
-                    
-    parser.add_argument(
-        '-ol',
-        '--out_lang',
-        metavar='output_language',
-        choices=['json', 'yaml', 'pickle', 'toml'],
-        default='json',
-        type=str,
-        help='Override language for output file'
+
+        parser.add_argument(
+            "-il",
+            "--in_lang",
+            metavar="input_language",
+            choices=["json", "yaml", "pickle", "toml"],
+            default="json",
+            type=str,
+            help="Override language for input file",
         )
-              
-    parser.add_argument(
-        'output_path',
-        metavar='output_path',
-        #required=True,
-        type=str,
-        help="Path to the file to be converted"
-        )      
-        
-    args = parser.parse_args()
-    converter.file_converter(**vars(args))
-except Exception as ex:
-    logging.warning(f"Warning: {ex}")
-    
+
+        parser.add_argument(
+            "-ol",
+            "--out_lang",
+            metavar="output_language",
+            choices=["json", "yaml", "pickle", "toml"],
+            default="json",
+            type=str,
+            help="Override language for output file",
+        )
+
+        parser.add_argument(
+            "output_path",
+            metavar="output_path",
+            # required=True,
+            type=str,
+            help="Path to the file to be converted",
+        )
+
+        args = parser.parse_args()
+        converter.file_converter(**vars(args))
+    except Exception as ex:
+        logging.warning(f"Warning: {ex}")
+
+
+
--- create_serializer.py
+++ create_serializer.py
@@ -2,28 +2,28 @@ from ser_create.json_serializer import JsonSerializer
 from ser_create.pickle_serializer import PickleSerializer
 from ser_create.yaml_serializer import YamlSerializer
 from ser_create.toml_serializer import TomlSerializer
-    
+
 
 def create_serializer(ser_lang):
-    if ser_lang == 'json':
+    if ser_lang == "json":
         return JsonSerializer()
-    if ser_lang == 'pickle':
+    if ser_lang == "pickle":
         return PickleSerializer()
-    if ser_lang == 'toml':
+    if ser_lang == "toml":
         return TomlSerializer()
-    if ser_lang == 'yaml':
+    if ser_lang == "yaml":
         return YamlSerializer()
-    raise TypeError('Invalid serializer name1')
-    
-    
+    raise TypeError("Invalid serializer name1")
+
+
 def create_serializer_file(ser_lang, in_file=""):
-     
+
     if in_file == "":
         return create_serializer(ser_lang)
-    else: 
+    else:
         file_ext = in_file.rpartition(".")[2]
         if file_ext == ser_lang:
             return create_serializer(ser_lang)
         else:
             return create_serializer(file_ext)
-    raise TypeError('Invalid serializer name2')
+    raise TypeError("Invalid serializer name2")
